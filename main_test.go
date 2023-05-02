bf7a1285c69fc9700cdeea61f83850719ab713f7
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2023-05-02 18:34:04 +0200

    Add testscript setup

--- main_test.go
+++ main_test.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
 	"testing"
+	"time"
 
-	qt "github.com/frankban/quicktest"
+	"github.com/bep/helpers/envhelpers"
+	"github.com/rogpeppe/go-internal/testscript"
 )
 
-func TestFoo(t *testing.T) {
-	t.Parallel()
+func TestScripts(t *testing.T) {
+	setup := testSetupFunc()
+	testscript.Run(t, testscript.Params{
+		Dir: "testscripts",
+		// UpdateScripts: true, // Uncomment to rewrite the test scripts with
+		// TestWork: true, // Uncomment to keep the test work dir.
+		Setup: func(env *testscript.Env) error {
+			return setup(env)
+		},
+	})
+}
 
-	c := qt.New(t)
+func TestMain(m *testing.M) {
+	os.Exit(
+		testscript.RunMain(m, map[string]func() int{
+			"gomaintemplate": func() int {
+				fmt.Println(strings.Join(os.Args[1:], " "))
+				time.Sleep(10 * time.Millisecond)
+				return 0
+			},
+		}),
+	)
+}
 
-	c.Assert(true, qt.IsTrue)
+func testSetupFunc() func(env *testscript.Env) error {
+	sourceDir, _ := os.Getwd()
+	return func(env *testscript.Env) error {
+		var keyVals []string
+		// Add some environment variables to the test script.
+		keyVals = append(keyVals, "SOURCE", sourceDir)
+		envhelpers.SetEnvVars(&env.Vars, keyVals...)
+
+		return nil
+	}
 }
